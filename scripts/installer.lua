600e24a4b65fbd457d68718749e741328f7cbe11
Naïm V <35503742+NaVAWU@users.noreply.github.com>
2026-05-12 09:56:21 +0200

    Fixed installer location for repo

--- scripts/installer.lua
+++ scripts/installer.lua
@@ -1,12 +1,12 @@
 -- installer.lua — run this once on the CC computer
 local files = {
-    ["config.lua"]  = "https://raw.githubusercontent.com/navawu/atm10_flight_controller/main/scripts/config.lua",
-    ["main.lua"]    = "https://raw.githubusercontent.com/navawu/atm10_flight_controller/main/scripts/main.lua",
-    ["motor.lua"]   = "https://raw.githubusercontent.com/navawu/atm10_flight_controller/main/scripts/motor.lua",
-    ["network.lua"] = "https://raw.githubusercontent.com/navawu/atm10_flight_controller/main/scripts/network.lua",
-    ["pid.lua"]     = "https://raw.githubusercontent.com/navawu/atm10_flight_controller/main/scripts/pid.lua",
-    ["sensors.lua"] = "https://raw.githubusercontent.com/navawu/atm10_flight_controller/main/scripts/sensors.lua",
-    ["state.lua"]   = "https://raw.githubusercontent.com/navawu/atm10_flight_controller/main/scripts/state.lua",
+    ["config.lua"]  = "https://raw.githubusercontent.com/NaVAWU/Flight-Controller/refs/heads/main/scripts/config.lua",
+    ["main.lua"]    = "https://raw.githubusercontent.com/NaVAWU/Flight-Controller/refs/heads/main/scripts/main.lua",
+    ["motor.lua"]   = "https://raw.githubusercontent.com/NaVAWU/Flight-Controller/refs/heads/main/scripts/motor.lua",
+    ["network.lua"] = "https://raw.githubusercontent.com/NaVAWU/Flight-Controller/refs/heads/main/scripts/network.lua",
+    ["pid.lua"]     = "https://raw.githubusercontent.com/NaVAWU/Flight-Controller/refs/heads/main/scripts/pid.lua",
+    ["sensors.lua"] = "https://raw.githubusercontent.com/NaVAWU/Flight-Controller/refs/heads/main/scripts/sensors.lua",
+    ["state.lua"]   = "https://raw.githubusercontent.com/NaVAWU/Flight-Controller/refs/heads/main/scripts/state.lua",
 }
 
 for filename, url in pairs(files) do
